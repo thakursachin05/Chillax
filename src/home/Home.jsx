59e75060e35449612a35591bd0a52045ecdccb30
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-06-26 18:02:43 +0530

    Update Home.jsx

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,10 +1,68 @@
+import Navbar from "../components/navbar/Navbar";
 import "./home.scss";
-import React from 'react'
+// import React from 'react'
+// import { AcUnit } from "@material-ui/icons";
 
 const Home = () => {
   return (
     <div className='home'>
-        Home
+        <Navbar />
+        <img
+          width="100%"
+            src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            alt=""
+          />
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum voluptates ea dolor modi, quos voluptas repudiandae excepturi vel, laboriosam doloremque perferendis veniam molestias doloribus, iste autem iure a. Voluptatum, quis!
     </div>
   )
 }
