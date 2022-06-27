89f674b5e74e8e9975584424c90f21b6a662dc40
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-06-27 15:01:58 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,17 +2,25 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
 
 dotenv.config();
 
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
   .then(() => console.log("DB CONNECT SUCCESSFUL!!"))
   .catch((err) => console.log(err.message));
 
-app.listen(8000, () => {
+
+app.use(express.json());
+
+app.use("/api/auth" , authRoute);
+app.use("/api/users" , userRoute);
+
+app.listen(8800, () => {
   console.log("Backend Server is running!");
 });
