ebd198bc015e44021a1798a3df1f64dd5e0d26c4
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-06-27 21:49:42 +0530

    Update Navbar.jsx

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import { Search, Notifications, ArrowDropDown } from "@material-ui/icons";
 import { useState } from "react";
 import "./navbar.scss";
+import {Link} from "react-router-dom";
 // import React from 'react'
 
 const Navbar = () => {
@@ -19,9 +20,17 @@ const Navbar = () => {
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
             alt=""
           />
-          <span>Homepage</span>
-          <span>Series</span>
-          <span>Movies</span>
+          <Link to="/" className="link">
+              <span>Homepage</span>
+          </Link>
+
+          <Link to="/series" className="link">
+            <span>Series</span>
+          </Link>
+
+          <Link to="/movies" className="link">
+            <span>Movies</span>
+          </Link>
           <span>New and Popular</span>
           <span>My List</span>
         </div>
