e7e41cdb3b1d02b9c4c49f63e98564c437eaa700
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-06-28 10:01:32 +0530

    Update Watch.jsx

--- client/src/pages/watch/Watch.jsx
+++ client/src/pages/watch/Watch.jsx
@@ -1,21 +1,19 @@
-// import React from 'react'
 import { ArrowBackOutlined } from "@material-ui/icons";
+import { Link, useLocation } from "react-router-dom";
 import "./watch.scss";
 
 export default function Watch() {
+  const location = useLocation();
+  const movie = location.movie;
   return (
     <div className="watch">
-      <div className="back">
-        <ArrowBackOutlined />
-        Home
-      </div>
-      <video
-        className="video"
-        autoPlay
-        progress
-        controls
-        src="https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761"
-      />
+      <Link to="/">
+        <div className="back">
+          <ArrowBackOutlined />
+          Home
+        </div>
+      </Link>
+      <video className="video" autoPlay progress controls src={movie.video} />
     </div>
   );
 }
