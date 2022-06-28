7992b5e697ed6bbc7198b6dc2fb8edeed890cf81
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-06-28 10:01:36 +0530

    Update List.jsx

--- client/src/components/list/List.jsx
+++ client/src/components/list/List.jsx
@@ -5,7 +5,7 @@ import { useRef } from "react";
 import ListItem from "../listItem/ListItem";
 import "./list.scss";
 
-export default function List() {
+export default function List({list}) {
 
     const [isMoved, setIsMoved ] = useState(false);
     const [slideNumber , setSlideNumber] = useState(0);
@@ -35,20 +35,13 @@ export default function List() {
 
   return (
     <div className="list">
-        <span className="listTitle">Continue to watch</span>
+        <span className="listTitle">{list.title}</span>
         <div className="wrapper">
             <ArrowBackIosOutlined className="sliderArrow left" onClick={()=>handleClick("left")} style={{display : !isMoved && "none"}} />
             <div className="container" ref={listRef}>
-                <ListItem index={0}/>
-                <ListItem index={1}/>
-                <ListItem index={2}/>
-                <ListItem index={3}/>
-                <ListItem index={4}/>
-                <ListItem index={5}/>
-                <ListItem index={6}/>
-                <ListItem index={7}/>
-                <ListItem index={8}/>
-                <ListItem index={9}/>
+              {list.content.map((item,i) =>{
+                <ListItem index={i} item={item} />
+              })}
             </div>
             <ArrowForwardIosOutlined className="sliderArrow right"  onClick={()=>handleClick("right")}/>
         </div>
