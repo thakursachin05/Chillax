e7888795d91d9bae84a497b40cb0edd325d33574
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-06-28 13:42:51 +0530

    Create AuthActions.js

---
+++ admin/src/context/authContext/AuthActions.js
@@ -0,0 +1,10 @@
+export const loginStart = () => ({
+    type : "LOGIN_START",
+});
+export const loginSuccess = (user) => ({
+    type : "LOGIN_SUCESS",
+    payload : user
+});
+export const loginFailure = () => ({
+    type : "LOGIN_FAILURE",
+});
