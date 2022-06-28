18fe7d4bb7dcd9f263a1d4fca9a840b6123c62c4
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-06-28 16:45:37 +0530

    Update AuthContext.js

--- admin/src/context/authContext/AuthContext.js
+++ admin/src/context/authContext/AuthContext.js
@@ -1,8 +1,9 @@
+import { useEffect } from "react";
 import { createContext, useReducer } from "react";
 import AuthReducer from "./AuthReducers";
 
 const INITIAL_STATE = {
-  user: null,
+  user: JSON.parse(localStorage.getItem("user")) || null,
   isFetching: false,
   error: false,
 };
@@ -11,17 +12,19 @@ export const AuthContext = createContext(INITIAL_STATE);
 
 export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
-
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(state.user));
+  }, [state.user]);
   return (
-  <AuthContext.Provider
-    value={{
-      user: state.user,
-      isFetching: state.isFetching,
-      error: state.error,
-      dispatch,
-    }}
-  >
-    {children}
-  </AuthContext.Provider>
-  )
+    <AuthContext.Provider
+      value={{
+        user: state.user,
+        isFetching: state.isFetching,
+        error: state.error,
+        dispatch,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 };
