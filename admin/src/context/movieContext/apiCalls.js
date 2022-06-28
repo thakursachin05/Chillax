17bd9059fe8ea4a20fce6fa2d1db65aaba74996e
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-06-28 16:45:44 +0530

    Create apiCalls.js

---
+++ admin/src/context/movieContext/apiCalls.js
@@ -0,0 +1,16 @@
+import { getMoviesFailure, getMoviesStart, getMoviesSuccess } from "./MovieActions";
+import axios from "axios";
+
+export const getMovies = async (dispatch) => {
+  dispatch(getMoviesStart());
+  try {
+    const res = await axios.get("/movies", {
+      headers: {
+        token: "Bearer " + JSON.parse(localStorage.getItem("user")).accessToken,
+      },
+    });
+    dispatch(getMoviesSuccess(res.data));
+  } catch (err) {
+    dispatch(getMoviesFailure());
+  }
+};
