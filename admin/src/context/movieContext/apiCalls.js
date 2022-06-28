51aa0def9465820e26c92f5f9c84574a2b9d5f74
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-06-28 22:44:18 +0530

    Update apiCalls.js

--- admin/src/context/movieContext/apiCalls.js
+++ admin/src/context/movieContext/apiCalls.js
@@ -1,5 +1,15 @@
-import { getMoviesFailure, getMoviesStart, getMoviesSuccess } from "./MovieActions";
 import axios from "axios";
+import {
+  createMovieFailure,
+  createMovieStart,
+  createMovieSuccess,
+  deleteMovieFailure,
+  deleteMovieStart,
+  deleteMovieSuccess,
+  getMoviesFailure,
+  getMoviesStart,
+  getMoviesSuccess,
+} from "./MovieActions";
 
 export const getMovies = async (dispatch) => {
   dispatch(getMoviesStart());
@@ -12,5 +22,35 @@ export const getMovies = async (dispatch) => {
     dispatch(getMoviesSuccess(res.data));
   } catch (err) {
     dispatch(getMoviesFailure());
+  }
+};
+
+//create
+export const createMovie = async (movie, dispatch) => {
+  dispatch(createMovieStart());
+  try {
+    const res = await axios.post("/movies", movie, {
+      headers: {
+        token: "Bearer " + JSON.parse(localStorage.getItem("user")).accessToken,
+      },
+    });
+    dispatch(createMovieSuccess(res.data));
+  } catch (err) {
+    dispatch(createMovieFailure());
+  }
+};
+
+//delete
+export const deleteMovie = async (id, dispatch) => {
+  dispatch(deleteMovieStart());
+  try {
+    await axios.delete("/movies/" + id, {
+      headers: {
+        token: "Bearer " + JSON.parse(localStorage.getItem("user")).accessToken,
+      },
+    });
+    dispatch(deleteMovieSuccess(id));
+  } catch (err) {
+    dispatch(deleteMovieFailure());
   }
 };
