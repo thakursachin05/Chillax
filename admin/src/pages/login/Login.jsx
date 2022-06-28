d0934a14e74be14999517b1d3288d07d30d47c3b
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-06-28 14:14:48 +0530

    Update Login.jsx

--- admin/src/pages/login/Login.jsx
+++ admin/src/pages/login/Login.jsx
@@ -1,24 +1,40 @@
-import React, { useContext, useState } from 'react'
-import { login } from '../../context/authContext/apiCalls';
-import { AuthContext } from '../../context/authContext/AuthContext';
-import "./login.css"
+import React, { useContext, useState } from "react";
+import { login } from "../../context/authContext/apiCalls";
+import { AuthContext } from "../../context/authContext/AuthContext";
+import "./login.css";
 
 export default function Login() {
-    const [email,setEmail] = useState("");
-    const [password,setPassword] = useState("");
-    const [isFetching,dispatch] = useContext(AuthContext);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isFetching, dispatch] = useContext(AuthContext);
 
-    const handleLogin = (e) => {
-        e.preventDefault();
-        login({email,password},dispatch)
-    }
+  const handleLogin = (e) => {
+    e.preventDefault();
+    login({ email, password }, dispatch);
+  };
   return (
     <div className="login">
-        <form action="" className="loginForm">
-            <input type="text" placeholder="email" className="loginInput" onChange={(e)=>setEmail(e.target.value)}/>
-            <input type="password" placeholder="password" className="loginInput"  onChange={(e)=>setPassword(e.target.value)} />
-            <button className="loginButton" onClick={handleLogin} disable={isFetching}>Login</button>
-        </form>    
+      <form action="" className="loginForm">
+        <input
+          type="text"
+          placeholder="email"
+          className="loginInput"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="loginInput"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button
+          className="loginButton"
+          onClick={handleLogin}
+          disable={isFetching}
+        >
+          Login
+        </button>
+      </form>
     </div>
-  )
+  );
 }
