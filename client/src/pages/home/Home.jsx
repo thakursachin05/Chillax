d2cccacad9bb8647519cc401baef17d945a98368
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-06-28 10:01:28 +0530

    Update Home.jsx

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -2,8 +2,7 @@ import Navbar from "../../components/navbar/Navbar";
 import Featured from "../../components/featured/Featured";
 import "./home.scss";
 import List from "../../components/list/List";
-import { useState } from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 const Home = ({ type }) => {
@@ -20,31 +19,25 @@ const Home = ({ type }) => {
           {
             headers: {
               token:
-                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyYjk2MDU0MmFlZTRhNmQwZjViZDE2MSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1NjMyNTI0NSwiZXhwIjoxNjU2NzU3MjQ1fQ.JZKpLZdqzXW1f-5Zd7QSOkUwWn3KW9dplwqGszhcyBs",
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyYjk2MDU0MmFlZTRhNmQwZjViZDE2MSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1NjMyNTI0NSwiZXhwIjoxNjU2NzU3MjQ1fQ.JZKpLZdqzXW1f-5Zd7QSOkUwWn3KW9dplwqGszhcyBs",
             },
           }
         );
-        console.log(res);
-        // setLists(res.data);
+        setLists(res.data);
       } catch (err) {
         console.log(err);
       }
     };
     getRandomLists();
   }, [type, genre]);
+
   return (
     <div className="home">
       <Navbar />
-      {/* <img
-          width="100%"
-            src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-            alt=""
-          /> */}
-      <Featured type={type} />
-      <List />
-      <List />
-      <List />
-      <List />
+      <Featured type={type} setGenre={setGenre} />
+      {lists.map((list) => (
+        <List list={list} />
+      ))}
     </div>
   );
 };
