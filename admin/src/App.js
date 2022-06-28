8f70e10cd87b6a61818769efc81282fb732f9b15
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-06-28 22:44:08 +0530

    Update App.js

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,56 +1,70 @@
 import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
-import "./App.css";
+import "./app.css";
 import Home from "./pages/home/Home";
-import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import UserList from "./pages/userList/UserList";
 import User from "./pages/user/User";
 import NewUser from "./pages/newUser/NewUser";
-import ProductList from "./pages/productList/ProductList";
-import Product from "./pages/product/Product";
-import NewProduct from "./pages/newProduct/NewProduct";
 import Login from "./pages/login/Login";
-import { useContext } from "react";
 import { AuthContext } from "./context/authContext/AuthContext";
-
+import { useContext } from "react";
+// import ListList from "./pages/listList/ListList";
+// import List from "./pages/list/List";
+// import NewList from "./pages/newList/NewList";
+import MovieList from "./pages/movieList/MovieList";
+import { Movie } from "@material-ui/icons";
+import NewMovie from "./pages/newMovie/NewMovie";
 
 function App() {
   const { user } = useContext(AuthContext);
   return (
     <Router>
       <Switch>
-          <Route  path="/login">
-            {user ? <Redirect to="/" /> : <Login />}
-          </Route>
-          {user &&
+        <Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route>
+        {user && (
           <>
-      <Topbar />
-      <div className="container">
-        <Sidebar />
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/users">
-            <UserList />
-          </Route>
-          <Route path="/user/:userId">
-            <User />
-          </Route>
-          <Route path="/newUser">
-            <NewUser />
-          </Route>
-          <Route path="/movies">
-            <ProductList />
-          </Route>
-          <Route path="/product/:productId">
-            <Product />
-          </Route>
-          <Route path="/newproduct">
-            <NewProduct />
-          </Route>
-      </div>
+            <Topbar />
+            <div className="container">
+              <Sidebar />
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route path="/users">
+                <UserList />
+              </Route>
+              <Route path="/user/:userId">
+                <User />
+              </Route>
+              <Route path="/newUser">
+                <NewUser />
+              </Route>
+              <Route path="/movies">
+                <MovieList />
+              </Route>
+              <Route path="/movie/:movieId">
+                <Movie />
+              </Route>
+              <Route path="/newMovie">
+                <NewMovie />
+              </Route>
+              <Route path="/lists">
+                <ListList />
+              </Route>
+              <Route path="/list/:listId">
+                <List />
+              </Route>
+              <Route path="/newlist">
+                <NewList />
+              </Route>
+            </div>
           </>
-        }
+        )}
       </Switch>
     </Router>
   );
