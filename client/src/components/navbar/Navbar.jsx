581f31a70454a4d3353827d677a8d4bf0acef0ef
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-07-03 17:26:59 +0530

    Update Navbar.jsx

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,17 +1,18 @@
-import { Search, Notifications, ArrowDropDown } from "@material-ui/icons";
-import { useState } from "react";
+import { ArrowDropDown, Notifications, Search } from "@material-ui/icons";
+import { useContext, useState } from "react";
 import "./navbar.scss";
-import {Link} from "react-router-dom";
-// import React from 'react'
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../authContext/AuthContext";
+import { logout } from "../../authContext/AuthActions";
 
 const Navbar = () => {
-    const [isScrolled, setIsScrolled] = useState(false);
-    // console.log(window.pageYOffset);
+  const [isScrolled, setIsScrolled] = useState(false);
+  const { dispatch } = useContext(AuthContext);
 
-    window.onscroll = () =>{
-      setIsScrolled(window.pageYOffset === 0 ? false : true);
-      return () => (window.onscroll = null);
-    };
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll = null);
+  };
   return (
     <div className={isScrolled ? "navbar scrolled" : "navbar"}>
       <div className="container">
@@ -21,33 +22,30 @@ const Navbar = () => {
             alt=""
           />
           <Link to="/" className="link">
-              <span>Homepage</span>
+            <span>Homepage</span>
           </Link>
-
           <Link to="/series" className="link">
-            <span>Series</span>
+            <span className="navbarmainLinks">Series</span>
           </Link>
-
           <Link to="/movies" className="link">
-            <span>Movies</span>
+            <span className="navbarmainLinks">Movies</span>
           </Link>
           <span>New and Popular</span>
           <span>My List</span>
         </div>
-
         <div className="right">
-          <Search className="icon"/>
+          <Search className="icon" />
           <span>KID</span>
-          <Notifications className="icon"/>
+          <Notifications className="icon" />
           <img
             src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
             alt=""
           />
           <div className="profile">
-            <ArrowDropDown className="icon"/>
+            <ArrowDropDown className="icon" />
             <div className="options">
-                <span>Setting</span>
-                <span>Logout</span>
+              <span>Settings</span>
+              <span onClick={() => dispatch(logout())}>Logout</span>
             </div>
           </div>
         </div>
