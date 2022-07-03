1105cf66d7dfec00245b309645b1f40927ff05f2
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-07-03 17:27:22 +0530

    Update Register.jsx

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,52 +1,68 @@
-import { useRef, useState } from "react";
+import axios from "axios";
+import { useRef } from "react";
+import { useState } from "react";
+import { Redirect, useHistory } from "react-router-dom";
 import "./register.scss";
 
 export default function Register() {
-    const [email,setEmail] = useState("");
-    const [password,setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const history = useHistory();
 
-    const emailRef = useRef();
-    const passwordRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const usernameRef = useRef();
 
-    const handleStart = () => {
-        setEmail(emailRef.current.value);
-    }
-    const handleFinish = () => {
-        setPassword(passwordRef.current.value);
-    }
+  const handleStart = () => {
+    setEmail(emailRef.current.value);
+  };
+  const handleFinish = async (e) => {
+    e.preventDefault();
+    setPassword(passwordRef.current.value);
+    setUsername(usernameRef.current.value);
+    try {
+      await axios.post("auth/register", { email,username, password });
+      history.push("/login");
+    } catch (err) {}
+  };
+  // const handleClick = async () => {
+    
+  // };
   return (
     <div className="register">
       <div className="top">
         <div className="wrapper">
-            <img
+          <img
+            className="logo"
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
             alt=""
-            className="logo"
-            />
-            <button className="loginButton">Sign In</button>
+          />
+          <button className="loginButton">Sign In</button>
         </div>
       </div>
       <div className="container">
-      <h1>Unlimited movies, TV shows, and more.</h1>
+        <h1>Unlimited movies, TV shows, and more.</h1>
         <h2>Watch anywhere. Cancel anytime.</h2>
         <p>
           Ready to watch? Enter your email to create or restart your membership.
         </p>
         {!email ? (
-            
-                <div className="input">
-                <input type="email" placeholder="Email address" ref={emailRef}/>
-                <button className="registerButton" onClick={handleStart}>
-                    Get Started
-                </button>
-            </div>
-            ) : ( <form className="input">
-            <input type="password" placeholder="password" ref={passwordRef}/>
-            <button className="registerButton" onClick={handleFinish}>
-               Start
+          <div className="input">
+            <input type="email" placeholder="email address" ref={emailRef} />
+            <button className="registerButton" onClick={handleStart}>
+              Get Started
             </button>
-        </form>)
-        }
+          </div>
+        ) : (
+          <form className="input">
+            <input type="username" placeholder="username" ref={usernameRef} />
+            <input type="password" placeholder="password" ref={passwordRef} />
+            <button className="registerButton" onClick={handleFinish}>
+              Start
+            </button>
+          </form>
+        )}
       </div>
     </div>
   );
