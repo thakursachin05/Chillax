91c05c81b42e293a57e047236b3a24a629de15ea
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-07-03 17:26:32 +0530

    Update List.jsx

--- client/src/components/list/List.jsx
+++ client/src/components/list/List.jsx
@@ -1,50 +1,49 @@
-// import React from 'react'
-import { ArrowBackIosOutlined, ArrowForwardIosOutlined } from "@material-ui/icons";
-import { useState } from "react";
-import { useRef } from "react";
+import {
+  ArrowBackIosOutlined,
+  ArrowForwardIosOutlined,
+} from "@material-ui/icons";
+import { useRef, useState } from "react";
 import ListItem from "../listItem/ListItem";
 import "./list.scss";
 
-export default function List({list}) {
+export default function List({ list }) {
+  const [isMoved, setIsMoved] = useState(false);
+  const [slideNumber, setSlideNumber] = useState(0);
+  const [clickLimit, setClickLimit] = useState(window.innerWidth / 230);
 
-    const [isMoved, setIsMoved ] = useState(false);
-    const [slideNumber , setSlideNumber] = useState(0);
+  const listRef = useRef();
 
-    const listRef = useRef()
-
-    const handleClick = (direction) => {
-        
-        let distance = listRef.current.getBoundingClientRect().x-50;
-       
-        if(direction==="left" && slideNumber>0){
-            // if(slideNumber === 1){
-            //     setIsMoved(false);
-            // My logic
-            // }
-            setSlideNumber(slideNumber-1);
-            listRef.current.style.transform =  `translateX(${230+distance}px)`;
-        }
-        
-        if(direction==="right" && slideNumber < 5){
-            setIsMoved(true);
-            setSlideNumber(slideNumber+1);
-            listRef.current.style.transform =  `translateX(${-230+distance}px)`;
-        }
-        // console.log(distance);
+  const handleClick = (direction) => {
+    setIsMoved(true);
+    let distance = listRef.current.getBoundingClientRect().x - 50;
+    if (direction === "left" && slideNumber > 0) {
+      setSlideNumber(slideNumber - 1);
+      listRef.current.style.transform = `translateX(${230 + distance}px)`;
     }
-
+    if (direction === "right" && slideNumber < 10 - clickLimit) {
+      setSlideNumber(slideNumber + 1);
+      listRef.current.style.transform = `translateX(${-230 + distance}px)`;
+    }
+  };
   return (
     <div className="list">
-        <span className="listTitle">{list.title}</span>
-        <div className="wrapper">
-            <ArrowBackIosOutlined className="sliderArrow left" onClick={()=>handleClick("left")} style={{display : !isMoved && "none"}} />
-            <div className="container" ref={listRef}>
-              {list.content.map((item,i) =>{
-                <ListItem index={i} item={item} />
-              })}
-            </div>
-            <ArrowForwardIosOutlined className="sliderArrow right"  onClick={()=>handleClick("right")}/>
+      <span className="listTitle">{list.title}</span>
+      <div className="wrapper">
+        <ArrowBackIosOutlined
+          className="sliderArrow left"
+          onClick={() => handleClick("left")}
+          style={{ display: !isMoved && "none" }}
+        />
+        <div className="container" ref={listRef}>
+          {list.content.map((item, i) => (
+            <ListItem index={i} item={item} />
+          ))}
         </div>
+        <ArrowForwardIosOutlined
+          className="sliderArrow right"
+          onClick={() => handleClick("right")}
+        />
+      </div>
     </div>
-  )
+  );
 }
