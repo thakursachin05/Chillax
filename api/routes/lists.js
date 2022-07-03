07a5b4849847211d61acde7aeec85081e9f12646
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-07-03 17:24:36 +0530

    Update lists.js

--- api/routes/lists.js
+++ api/routes/lists.js
@@ -2,47 +2,14 @@ const router = require("express").Router();
 const List = require("../models/List");
 const verify = require("../verifyToken");
 
-// Create
+//CREATE
 
 router.post("/", verify, async (req, res) => {
   if (req.user.isAdmin) {
     const newList = new List(req.body);
-
     try {
       const savedList = await newList.save();
       res.status(201).json(savedList);
-    } catch (err) {
-      res.status(500).json(err.message);
-    }
-  } else {
-    res.status(403).json("You are not allowed!");
-  }
-});
-// Update
-
-// router.put("/:id", verify, async (req, res) => {
-//   if (req.user.isAdmin) {
-//     try {
-//       const updatedMovie = await Movie.findByIdAndUpdate(
-//         req.params.id,
-//         { $set: req.body },
-//         { new: true }
-//       );
-//       res.status(200).json(updatedMovie);
-//     } catch (err) {
-//       res.status(500).json(err);
-//     }
-//   } else {
-//     res.status(403).json("You are not allowed!");
-//   }
-// });
-//  Delete
-
-router.delete("/:id", verify, async (req, res) => {
-  if (req.user.isAdmin) {
-    try {
-      await Movie.findByIdAndDelete(req.params.id);
-      res.status(200).json("The list has been deleted");
     } catch (err) {
       res.status(500).json(err);
     }
@@ -51,41 +18,47 @@ router.delete("/:id", verify, async (req, res) => {
   }
 });
 
-// Get
+//DELETE
 
-router.get("/", verify , async (req,res)=>{
-    const typeQuery = req.query.type;
-    const genreQuery = req.query.genre;
-
-    let list = [];
-
+router.delete("/:id", verify, async (req, res) => {
+  if (req.user.isAdmin) {
     try {
-        if(typeQuery){
-            if(genreQuery){
-                list = await List.aggregate([
-                    {$sample : { size : 10} },
-                    {$match : {type : typeQuery , genre: genreQuery}}
-                ])
-            }
-            else{
-                list = await List.aggregate([
-                    {$sample : { size : 10} },
-                    {$match : {type : typeQuery }}
-                ])
-            }
-        }
-        else{
-            list = await List.aggregate([
-                {$sample : { size : 10} }
-            ])
-        }
-
-        res.status(200).json(list);
-        
+      await List.findByIdAndDelete(req.params.id);
+      res.status(201).json("The list has been delete...");
     } catch (err) {
-        res.status(500).json(arr);
+      res.status(500).json(err);
     }
-})
+  } else {
+    res.status(403).json("You are not allowed!");
+  }
+});
 
+//GET
+
+router.get("/", verify, async (req, res) => {
+  const typeQuery = req.query.type;
+  const genreQuery = req.query.genre;
+  let list = [];
+  try {
+    if (typeQuery) {
+      if (genreQuery) {
+        list = await List.aggregate([
+          { $sample: { size: 10 } },
+          { $match: { type: typeQuery, genre: genreQuery } },
+        ]);
+      } else {
+        list = await List.aggregate([
+          { $sample: { size: 10 } },
+          { $match: { type: typeQuery } },
+        ]);
+      }
+    } else {
+      list = await List.aggregate([{ $sample: { size: 10 } }]);
+    }
+    res.status(200).json(list);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
