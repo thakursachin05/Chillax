f66be9c33b1fd47930aebd57a5bdc9b95a1970a4
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-07-03 17:27:19 +0530

    Update Login.jsx

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,33 +1,53 @@
-
+import { useContext, useState } from "react";
+import { login } from "../../authContext/apiCalls";
+import { AuthContext } from "../../authContext/AuthContext";
 import "./login.scss";
 
 export default function Login() {
-  
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { dispatch } = useContext(AuthContext);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    login({ email, password }, dispatch);
+  };
   return (
     <div className="login">
       <div className="top">
         <div className="wrapper">
-            <img
+          <img
+            className="logo"
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
             alt=""
-            className="logo"
-            />
+          />
         </div>
       </div>
       <div className="container">
         <form>
           <h1>Sign In</h1>
-          <input type="email" placeholder="Email or Phone number" />
-          <input type="password" placeholder="Password" />
-          <button className="loginButton">Sign In</button>
-          <span>New to NetFlix ? <b>Sign up now.</b></span>
+          <input
+            type="email"
+            placeholder="Email or phone number"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button className="loginButton" onClick={handleLogin}>
+            Sign In
+          </button>
+          <span>
+            New to Netflix? <b>Sign up now.</b>
+          </span>
           <small>
             This page is protected by Google reCAPTCHA to ensure you're not a
             bot. <b>Learn more</b>.
           </small>
         </form>
       </div>
-    
     </div>
   );
 }
