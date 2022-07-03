11b7b85d469356c4d7c1df83dcc13bfdc311f4a8
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-07-03 17:24:42 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,30 @@
 import "./app.scss";
-import Home from './pages/home/Home';
-import Login from "./pages/login/Login";
+import Home from "./pages/home/Home";
 import Register from "./pages/register/Register";
 import Watch from "./pages/watch/Watch";
+import Login from "./pages/login/Login";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Redirect,
 } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./authContext/AuthContext";
 
 const App = () => {
-  const user = true;
+  const { user } = useContext(AuthContext);
   return (
     <Router>
-
-    <Switch>
-      <Route exact path="/">
-       { user ? <Home /> : <Redirect to="register" /> }
-      </Route>
-      <Route path="/register">
-      { !user ? <Register /> : <Redirect to="/" /> }
-      </Route>
-      <Route path="/login">
-      { !user ? <Login /> : <Redirect to="register" /> }
-      </Route>
-      {user && (
+      <Switch>
+        <Route exact path="/">
+          {user ? <Home /> : <Redirect to="/register" />}
+        </Route>
+        <Route path="/register">
+          {!user ? <Register /> : <Redirect to="/" />}
+        </Route>
+        <Route path="/login">{!user ? <Login /> : <Redirect to="/" />}</Route>
+        {user && (
           <>
             <Route path="/movies">
               <Home type="movies" />
@@ -41,6 +40,6 @@ const App = () => {
       </Switch>
     </Router>
   );
-}
+};
 
 export default App;
