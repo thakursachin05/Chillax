5459b7b11be57e8d294087631f42708de9514e84
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-07-03 17:26:29 +0530

    Update Featured.jsx

--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -1,76 +1,69 @@
-// import React from 'react'
 import { InfoOutlined, PlayArrow } from "@material-ui/icons";
 import axios from "axios";
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./featured.scss";
 
-export default function Featured({type}) {
-
+export default function Featured({ type, setGenre }) {
   const [content, setContent] = useState({});
 
   useEffect(() => {
-      const getRandomContent = async () => {
-        try {
-          const res = await axios.get(`/movies/random?type=${type}`,{
+    const getRandomContent = async () => {
+      try {
+        const res = await axios.get(`/movies/random?type=${type}`, {
           headers: {
             token:
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyYjk2MDU0MmFlZTRhNmQwZjViZDE2MSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1NjMyNTI0NSwiZXhwIjoxNjU2NzU3MjQ1fQ.JZKpLZdqzXW1f-5Zd7QSOkUwWn3KW9dplwqGszhcyBs",
+              "Bearer "+JSON.parse(localStorage.getItem("user")).accessToken,
           },
         });
-          setContent(res.data[0]);
-        } catch (err) {
-          console.log(err);
-        }
+        setContent(res.data[0]);
+      } catch (err) {
+        console.log(err);
       }
-      getRandomContent();
-  }, [type])
-  
+    };
+    getRandomContent();
+  }, [type]);
 
+  console.log(content);
   return (
     <div className="featured">
-        {type &&
-            <div className="category">
-                <span>{type === "movies" ? "Movies" : "Series"}</span>
-                <select name="genre" id="genre">
-                    <option>Genre</option>
-                    <option value="adventure">Adventure</option>
-                    <option value="comedy">Comedy</option>
-                    <option value="crime">Crime</option>
-                    <option value="fantasy">Fantasy</option>
-                    <option value="historical">Historical</option>
-                    <option value="horror">Horror</option>
-                    <option value="romance">Romance</option>
-                    <option value="sci-fi">Sci-fi</option>
-                    <option value="thriller">Thriller</option>
-                    <option value="western">Western</option>
-                    <option value="animation">Animation</option>
-                    <option value="drama">Drama</option>
-                    <option value="documentary">Documentary</option>
-                </select>
-            </div>
-        }
-      <img
-        src={content.img}
-        alt=""
-      />
+      {type && (
+        <div className="category">
+          <span>{type === "movies" ? "Movies" : "Series"}</span>
+          <select
+            name="genre"
+            id="genre"
+            onChange={(e) => setGenre(e.target.value)}
+          >
+            <option>Genre</option>
+            <option value="adventure">Adventure</option>
+            <option value="comedy">Comedy</option>
+            <option value="crime">Crime</option>
+            <option value="fantasy">Fantasy</option>
+            <option value="historical">Historical</option>
+            <option value="horror">Horror</option>
+            <option value="romance">Romance</option>
+            <option value="sci-fi">Sci-fi</option>
+            <option value="thriller">Thriller</option>
+            <option value="western">Western</option>
+            <option value="animation">Animation</option>
+            <option value="drama">Drama</option>
+            <option value="documentary">Documentary</option>
+          </select>
+        </div>
+      )}
+      <img src={content.img} alt="" />
       <div className="info">
-        <img
-          src={content.imgTitle}
-          alt=""
-        />
-        <span className="desc">
-         {content.desc}
-        </span>
+        <img src={content.imgTitle} alt="" />
+        <span className="desc">{content.desc}</span>
         <div className="buttons">
-            <button className="play">
-                <PlayArrow />
-                <span>Play</span>
-            </button>
-            
-            <button className="more">
-                <InfoOutlined />
-                <span>Info</span>
-            </button>
+          <button className="play">
+            <PlayArrow />
+            <span>Play</span>
+          </button>
+          <button className="more">
+            <InfoOutlined />
+            <span>Info</span>
+          </button>
         </div>
       </div>
     </div>
