e7c881f52e9aa8ee964b317e426807b864e8d257
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-07-03 17:24:34 +0530

    Update auth.js

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -2,10 +2,8 @@ const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
 const jwt = require("jsonwebtoken");
-const verify = require("../verifyToken");
 
-//Register
-
+//REGISTER
 router.post("/register", async (req, res) => {
   const newUser = new User({
     username: req.body.username,
@@ -15,7 +13,6 @@ router.post("/register", async (req, res) => {
       process.env.SECRET_KEY
     ).toString(),
   });
-
   try {
     const user = await newUser.save();
     res.status(201).json(user);
@@ -25,17 +22,16 @@ router.post("/register", async (req, res) => {
 });
 
 //LOGIN
-
 router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ email: req.body.email });
-    !user && res.status(401).json("Wrong password or username!!!");
+    !user && res.status(401).json("Wrong password or username!");
 
     const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
     const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
 
     originalPassword !== req.body.password &&
-      res.status(401).json("Wrong password or username!!!");
+      res.status(401).json("Wrong password or username!");
 
     const accessToken = jwt.sign(
       { id: user._id, isAdmin: user.isAdmin },
@@ -45,7 +41,7 @@ router.post("/login", async (req, res) => {
 
     const { password, ...info } = user._doc;
 
-    res.status(200).json({...info, accessToken});
+    res.status(200).json({ ...info, accessToken });
   } catch (err) {
     res.status(500).json(err);
   }
