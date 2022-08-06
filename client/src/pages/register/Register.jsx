389f23c11812e5d6698fc8a76b59d8a255eb8ef4
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-08-06 14:24:48 +0530

    Update Register.jsx

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -66,15 +66,15 @@ export default function Register() {
         </p>
         {!email ? (
           <div className="input">
-            <input type="email" placeholder="email address" ref={emailRef} />
+            <input type="email" placeholder="Email address" ref={emailRef} />
             <button className="registerButton" onClick={handleStart}>
               Get Started
             </button>
           </div>
         ) : (
           <form className="input">
-            <input type="username" placeholder="username" ref={usernameRef} />
-            <input type="password" placeholder="password" ref={passwordRef} />
+            <input type="username" placeholder="Username" ref={usernameRef} />
+            <input type="password" placeholder="Password" ref={passwordRef} />
             <button className="registerButton" onClick={handleFinish}>
               Start
             </button>
