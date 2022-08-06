2062d1e638bff116de34979f966f8b6fe490fc9a
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-08-06 12:13:26 +0530

    Update lists.js

--- api/routes/lists.js
+++ api/routes/lists.js
@@ -9,12 +9,12 @@ router.post("/", verify, async (req, res) => {
     const newList = new List(req.body);
     try {
       const savedList = await newList.save();
-      res.status(201).json(savedList);
+      return res.status(201).json(savedList);
     } catch (err) {
-      res.status(500).json(err);
+      return res.status(500).json(err);
     }
   } else {
-    res.status(403).json("You are not allowed!");
+   return res.status(403).json("You are not allowed!");
   }
 });
 
@@ -24,12 +24,12 @@ router.delete("/:id", verify, async (req, res) => {
   if (req.user.isAdmin) {
     try {
       await List.findByIdAndDelete(req.params.id);
-      res.status(201).json("The list has been delete...");
+      return res.status(201).json("The list has been delete...");
     } catch (err) {
-      res.status(500).json(err);
+      return res.status(500).json(err);
     }
   } else {
-    res.status(403).json("You are not allowed!");
+    return res.status(403).json("You are not allowed!");
   }
 });
 
@@ -55,9 +55,9 @@ router.get("/", verify, async (req, res) => {
     } else {
       list = await List.aggregate([{ $sample: { size: 10 } }]);
     }
-    res.status(200).json(list);
+    return res.status(200).json(list);
   } catch (err) {
-    res.status(500).json(err);
+    return res.status(500).json(err);
   }
 });
 
