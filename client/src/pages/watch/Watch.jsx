9bd7b2dc96d258039b881662255e4393ff0a9fd1
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-08-06 12:14:51 +0530

    Update Watch.jsx

--- client/src/pages/watch/Watch.jsx
+++ client/src/pages/watch/Watch.jsx
@@ -5,6 +5,7 @@ import "./watch.scss";
 export default function Watch() {
   const location = useLocation();
   const movie = location.movie;
+  console.log(movie);
   return (
     <div className="watch">
       <Link to="/">
@@ -13,7 +14,20 @@ export default function Watch() {
           Home
         </div>
       </Link>
-      <video className="video" autoPlay progress controls src={movie.video} />
+      {/* <video className="video" autoPlay progress controls src={movie.video} /> */}
+      {/* <iframe
+        width="560"
+        height="315"
+        src={movie.video}
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe> */}
+      {/* <Link to={movie.video}> */}
+        {/* Miegw */}
+      {/* </Link> */}
+      {/* {console.log(movie.video)} */}
     </div>
   );
 }
